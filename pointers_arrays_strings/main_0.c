99ad60b2e222dbadfd0e982883d4cf7f19c83a98
smkhwanazi71 <spmkhwanazi71@gmail.com>
2023-02-28 03:58:39 -0800

    main _0.c

--- pointers_arrays_strings/main_0.c
+++ pointers_arrays_strings/main_0.c
@@ -1,12 +1,18 @@
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  int n;
+
+  n = 402;
+  printf("n=%d\n", n);
+  reset_to_98(&n);
+  printf("n=%d\n", n);
+  return (0);
+}
