cfe4a0863338bf226cf441d1cf2c3ddd5b924d97
smkhwanazi71 <spmkhwanazi71@gmail.com>
2023-03-22 01:46:52 -0700

    hello. holberton.

---
+++ variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .text
+	global _start
+_start:
+	mov rdx,len
+	mov rcx,msg
+	mov rbx,1
+	mov rax,3
+	int 0x80
+
+	mov eax,1
+	int 0x80
+
+section .data
+msg db 'Hello, Holberton', 0xa
+	len equ $ - msg
+	
