ba8dd849f0682f51a59ad2be20030c9e1542836d
smkhwanazi71 <spmkhwanazi71@gmail.com>
2023-03-22 03:45:37 -0700

    sum them all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,16 +9,16 @@
   */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, result;
-	va_list ap;
+unsigned int i, result;
+va_list ap;
 
-	if (n == 0)
-		return (0);
-	va_start(ap, n);
-	for (i = result = 0; i < n; i++)
-	{
-		result += va_arg(ap, int);
-	}
-	va_end(ap);
-	return (result);
+if (n == 0)
+return (0);
+va_start(ap, n);
+for (i = result = 0; i < n; i++)
+{
+result += va_arg(ap, int);
+}
+va_end(ap);
+return (result);
 }
