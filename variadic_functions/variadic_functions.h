f13c2779bf6304ce553a1ac580f969d3e1a0b15e
smkhwanazi71 <spmkhwanazi71@gmail.com>
2023-03-24 07:04:58 -0700

    create

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,18 +1,35 @@
-#ifndef VARIADIC_FNS
-#define VARIADIC_FNS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+/*
+* File: variadic_functions.h
+* Auth: simphiwe mkhwanazi
+* Desc: Header file containing prototypes for all functions
+*      used in the 0x0F-variadic_functions directory.
+*/
 
-typedef struct validTypes
-{
-	char *valid;
-	void (*f)();
-} v_types;
+#include <stdarg.h>
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void _printchar(va_list list);
+void _printstr(va_list list);
+void _printfloat(va_list list);
+void _printint(va_list list);
 
+
+/**
+* struct checker - A new struct type defining a checker.
+* @type: A type representing a data type.
+* @f: A function pointer to a function that check
+*  a data type corresponding to type.
+*/
+
+typedef struct checker
+{
+char *type;
+void (*f)();
+} checker;
 #endif
