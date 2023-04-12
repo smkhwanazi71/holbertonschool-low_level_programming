09ffa245a693921279b88ddf03483610cdd9139f
smkhwanazi71 <spmkhwanazi71@gmail.com>
2023-04-11 23:52:37 -0700

    djb2

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,18 +1,19 @@
 #include "hash_tables.h"
-
 /**
- * hash_djb2 - hash function implementing the djb2 algorithm
- * @str: string to hash
- * Return: hashed value
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hsh;
-int c;
+	unsigned long int hash;
+	int c;
 
-hsh = 5381;
-while ((c = *str++))
-
-hsh = ((hsh << 5) + hsh) +c;  /* hsh * 33 + c */
-return (hsh);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
